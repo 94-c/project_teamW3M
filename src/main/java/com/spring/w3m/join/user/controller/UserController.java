2bbfbc56ec26c33643aa52108346aa53350e377d
woonggi1 <dndrl1014@gmail.com>
2021-03-16 10:59:03 +0900

    마이페이지 / 회원가입 시 이메일 보내는  의존성 추가

--- src/main/java/com/spring/w3m/join/user/controller/UserController.java
+++ src/main/java/com/spring/w3m/join/user/controller/UserController.java
@@ -25,42 +25,59 @@ public class UserController {
 	@RequestMapping("/insertMember.do")
 	public String insert_member() {
 		return "login/insertMember";
-		
+
 	}
+
+	@RequestMapping("/mypage.do")
+	public String myPage() {
+		System.out.println("마이페이지 진입");
+
+		return "login/myPage";
+
+	}
+
+	@RequestMapping("/memberInfoUpdate.do")
+	public String memberInfoUpdate() {
+		System.out.println("회원 정보 변경 진입");
+
+		return "login/memberInfo";
+	}
+
 	@RequestMapping("/login_insert.do")
 	public String insert_success(UserVO vo) {
-		
 
-		if(vo.getUser_marketing_mail() == null) { // check box 가 Null 이면 false
+		if (vo.getUser_marketing_mail() == null) { // check box 가 Null 이면 false
 			vo.setUser_marketing_mail(false);
 		}
-		if(vo.getUser_marketing_sms() == null) { // check box 가 Null 이면 false
+		if (vo.getUser_marketing_sms() == null) { // check box 가 Null 이면 false
 			vo.setUser_marketing_sms(false);
 		}
-		
+
 		String emailFormat = vo.getUser_email(); // 이메일 형식 만들기 '@'
 		vo.setUser_email(emailFormat.replace(",", "@"));
-		
-		String phoneFormat = vo.getUser_phone();//폰 형식 '-'
-		vo.setUser_phone(phoneFormat.replace(",","-"));
-		
-		String birthdayFormat = vo.getUser_birthday();//폰 형식 '-'
-		vo.setUser_birthday(birthdayFormat.replace(",","-"));
-		
+
+		String phoneFormat = vo.getUser_phone();// 폰 형식 '-'
+		vo.setUser_phone(phoneFormat.replace(",", "-"));
+
+		String birthdayFormat = vo.getUser_birthday();// 폰 형식 '-'
+		vo.setUser_birthday(birthdayFormat.replace(",", "-"));
+		String a = vo.getUser_password();
 		vo.setUser_password(passEncoder.encode(vo.getUser_password()));
-		
+
 		System.out.println(vo.toString());
 		userService.insertUser(vo);
 		return "login/insert_success";
 	}
+
 	@RequestMapping("")
 	public String getUser(UserVO vo, UserDAO dao) {
 		vo = userService.getUser();
 		System.out.println(vo.toString());
 		return null;
-		
+
 	}
-	@RequestMapping(value ="/user_id_check.do", method = RequestMethod.POST)
+
+	@RequestMapping(value = "/user_id_check.do", method = RequestMethod.POST)
 	@ResponseBody
 	public int idCheck(@RequestBody String user_id) {
 		int check = userService.idCheck(user_id);
@@ -68,24 +85,25 @@ public class UserController {
 //		System.out.println(user_id);
 //		System.out.println(check);
 		return check;
-		
+
 	}
+
 	@ResponseBody
 	@RequestMapping("/send_sms.do")
 	public String sendSMS(@RequestBody String user_phoneAll) {
-		
-		 Random rand  = new Random();
-	        String numStr = "";
-	        for(int i=0; i<6; i++) {
-	            String ran = Integer.toString(rand.nextInt(10));
-	            numStr+=ran;
-	        }
 
-	        System.out.println("수신자 번호 : " + user_phoneAll);
-	        System.out.println("인증번호 : " + numStr);
-	        certificationSMS sms = new certificationSMS();
-	        sms.certifiedPhone(user_phoneAll, numStr);
-	        return numStr;
-	
+		Random rand = new Random();
+		String numStr = "";
+		for (int i = 0; i < 6; i++) {
+			String ran = Integer.toString(rand.nextInt(10));
+			numStr += ran;
+		}
+
+		System.out.println("수신자 번호 : " + user_phoneAll);
+		System.out.println("인증번호 : " + numStr);
+		certificationSMS sms = new certificationSMS();
+		sms.certifiedPhone(user_phoneAll, numStr);
+		return numStr;
+
 	}
 }
