2babd0dac8014229fa3bf845ff025cc9296a4643
woonggi1 <dndrl1212@naver.com>
2021-03-26 10:29:23 +0900

    장바구니 수량 완료

--- src/main/java/com/spring/w3m/cart/user/service/CartService.java
+++ src/main/java/com/spring/w3m/cart/user/service/CartService.java
@@ -7,5 +7,5 @@ import com.spring.w3m.cart.user.vo.CartVO;
 public interface CartService {
 
 	public List<CartVO> CartList(String user_id); // 카트 리스트
-	public void Cart_Order_Cnt_Update(CartVO vo); // 주문수량 변경
+	public int Cart_Order_Cnt_Update(CartVO vo); // 주문수량 변경
 }
