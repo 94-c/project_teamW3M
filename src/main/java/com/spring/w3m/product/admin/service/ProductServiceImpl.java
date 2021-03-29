d67535ba43fcaeb20f97d2313a99df8f920e3db7
joe <dytpq2528@gmail.com>
2021-03-29 11:00:43 +0900

    파일업로드 수정중...

--- src/main/java/com/spring/w3m/product/admin/service/ProductServiceImpl.java
+++ src/main/java/com/spring/w3m/product/admin/service/ProductServiceImpl.java
@@ -39,21 +39,21 @@ public class ProductServiceImpl implements ProductService{
 			MultipartFile image4, MultipartFile image5, MultipartFile image6, MultipartFile image7, MultipartFile image8, MultipartFile image9, 
 			MultipartFile image10) throws IOException {
 		
-			vo.setProd_title_image(singleUpload(prod_thumb));
-			vo.setProd_image1(singleUpload(image1));
-			vo.setProd_image2(singleUpload(image2));
-			vo.setProd_image3(singleUpload(image3));
-			vo.setProd_image4(singleUpload(image4));
-			vo.setProd_image5(singleUpload(image5));
-			vo.setProd_image6(singleUpload(image6));
-			vo.setProd_image7(singleUpload(image7));
-			vo.setProd_image8(singleUpload(image8));
-			vo.setProd_image9(singleUpload(image9));
-			vo.setProd_image10(singleUpload(image10));
+			vo.setProd_title_image(thumbUpload(vo, prod_thumb));
+//			vo.setProd_image1(singleUpload(image1));
+//			vo.setProd_image2(singleUpload(image2));
+//			vo.setProd_image3(singleUpload(image3));
+//			vo.setProd_image4(singleUpload(image4));
+//			vo.setProd_image5(singleUpload(image5));
+//			vo.setProd_image6(singleUpload(image6));
+//			vo.setProd_image7(singleUpload(image7));
+//			vo.setProd_image8(singleUpload(image8));
+//			vo.setProd_image9(singleUpload(image9));
+//			vo.setProd_image10(singleUpload(image10));
 			
 			String prod_code = vo.getProd_code(); //상품코드 받아서 카테고리분류하는 작업
 			sortCategory(vo, prod_code);
-			
+			System.out.println("insert======================="+vo.getProd_title_image());
 			dao.insertProduct(vo);
 	}
 	
@@ -62,45 +62,48 @@ public class ProductServiceImpl implements ProductService{
 			MultipartFile image4, MultipartFile image5, MultipartFile image6, MultipartFile image7, MultipartFile image8,
 			MultipartFile image9, MultipartFile image10) throws IOException {
 		
-		vo.setProd_title_image(singleUpload(prod_thumb));
-		vo.setProd_image1(singleUpload(image1));
-		vo.setProd_image2(singleUpload(image2));
-		vo.setProd_image3(singleUpload(image3));
-		vo.setProd_image4(singleUpload(image4));
-		vo.setProd_image5(singleUpload(image5));
-		vo.setProd_image6(singleUpload(image6));
-		vo.setProd_image7(singleUpload(image7));
-		vo.setProd_image8(singleUpload(image8));
-		vo.setProd_image9(singleUpload(image9));
-		vo.setProd_image10(singleUpload(image10));
+		vo.setProd_title_image(thumbUpload(vo, prod_thumb));
+//		vo.setProd_image1(singleUpload(image1));
+//		vo.setProd_image2(singleUpload(image2));
+//		vo.setProd_image3(singleUpload(image3));
+//		vo.setProd_image4(singleUpload(image4));
+//		vo.setProd_image5(singleUpload(image5));
+//		vo.setProd_image6(singleUpload(image6));
+//		vo.setProd_image7(singleUpload(image7));
+//		vo.setProd_image8(singleUpload(image8));
+//		vo.setProd_image9(singleUpload(image9));
+//		vo.setProd_image10(singleUpload(image10));
 		
 		String prod_code = vo.getProd_code(); //상품코드 받아서 카테고리분류하는 작업
 		sortCategory(vo, prod_code);
-		
+		System.out.println("update======================="+vo.getProd_title_image());
 		dao.updateProduct(vo);
 	}
 	
-	public String singleUpload(MultipartFile file) throws IOException {	
+	public String thumbUpload(ProductVO vo, MultipartFile file) throws IOException {				
 		
-		if(file == null) { //업로드한 파일이 없을 경우
-			return "";
-		}else {
 			InputStream ism = file.getInputStream();
 			String fileName = file.getOriginalFilename();
 			String contentType = file.getContentType();
 			long contentLength = file.getSize();
 			
 			awsS3.uploadProduct(ism, fileName, contentType, contentLength); //상품이미지 업로드
-			return "https://imageup.s3.ap-northeast-2.amazonaws.com/product/" + fileName;
-		}
+			if(fileName == "") {
+				System.out.println("if======================="+vo.getProd_title_image());				
+				return vo.getProd_title_image();
+			}else {
+				System.out.println("else======================="+vo.getProd_title_image());
+				return "https://imageup.s3.ap-northeast-2.amazonaws.com/product/" + fileName;
+			}		
+		
 	}
 	
 	public void sortCategory(ProductVO vo, String prod_code) { //카테고리분류작업
 		String[] category = prod_code.split("-");
-		vo.setProd_category1(category[0]); //MM
-		vo.setProd_category2(category[1]); //P
-		vo.setProd_category3(category[2]); //L
-		vo.setProd_category4(category[3]); //94
+		
+		for(int i=0; i<category.length; i++) {			
+			vo.setProd_category1(category[i]);			
+		}		
 	}
 
 }
