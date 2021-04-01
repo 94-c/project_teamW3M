ad10c0ca3a8c5d94b1f40773bb8ea11ee5b62efc
joe <dytpq2528@gmail.com>
2021-04-01 14:39:38 +0900

    쿨패치/핫펙/체온계 상품추가 작업중

--- src/main/java/com/spring/w3m/product/admin/dao/ProductDAO.java
+++ src/main/java/com/spring/w3m/product/admin/dao/ProductDAO.java
@@ -6,7 +6,6 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.spring.w3m.delivery.common.vo.DeliveryVO;
 import com.spring.w3m.paging.common.Pagination;
 import com.spring.w3m.paging.common.Search;
 import com.spring.w3m.product.admin.vo.ProductVO;
