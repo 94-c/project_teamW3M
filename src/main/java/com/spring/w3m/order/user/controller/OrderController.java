a7c97c66c72b66db8754fd3d74ad4935ce11a70f
woonggi1 <dndrl1212@naver.com>
2021-04-02 12:50:05 +0900

    매퍼 수정

--- src/main/java/com/spring/w3m/order/user/controller/OrderController.java
+++ src/main/java/com/spring/w3m/order/user/controller/OrderController.java
@@ -149,7 +149,9 @@ public class OrderController {
 			int a =orderService.insert_delivery(ordervo);
 			
 			System.out.println(a +"- 0이면 실패");
-			
+			//order_prod 테이블 - 주문상태를 결제완료 업데이트
+			//order_list 테이블 - 주문생태를 배송전 업데이트
+			//order
 			
 			
 			return "order/order_success";
